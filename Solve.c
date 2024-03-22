32cb2c14fa2a9cf97b31484552e6816b4bb82315
silvervs <silver.vs@outlook.com>
2024-03-21 18:46:37 -0600

    Problemas 2.0

--- Solve.c
+++ Solve.c
@@ -14,3 +14,17 @@ void *multiplicar(void *arg) {
     printf("El producto de %d y %d es: %d\n", a, b, resultado);
     pthread_exit(NULL);
 }
+
+int main() {
+    pthread_t hilo_suma, hilo_multiplicacion;
+
+    // Crear hilos
+    pthread_create(&hilo_suma, NULL, sumar, NULL);
+    pthread_create(&hilo_multiplicacion, NULL, multiplicar, NULL);
+
+    // Esperar a que los hilos terminen
+    pthread_join(hilo_suma, NULL);
+    pthread_join(hilo_multiplicacion, NULL);
+
+    return 0;
+}
